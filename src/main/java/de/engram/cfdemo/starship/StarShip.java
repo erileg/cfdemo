82733aa5e8fae5778a422a2518f85ba04b073ff0
erle <erik.legenhausen@gmail.com>
2018-04-27 07:54:35 +0200

    add search method basePath -> /api added hal-browser

--- src/main/java/de/engram/cfdemo/starship/StarShip.java
+++ src/main/java/de/engram/cfdemo/starship/StarShip.java
@@ -6,13 +6,15 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 @Data
 @NoArgsConstructor
 public class StarShip {
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue (strategy = GenerationType.AUTO)
     private Long id;
 
     private String name;
--- src/main/java/de/engram/cfdemo/starship/StarShipRepository.java
+++ src/main/java/de/engram/cfdemo/starship/StarShipRepository.java
@@ -3,4 +3,5 @@ package de.engram.cfdemo.starship;
 import org.springframework.data.repository.CrudRepository;
 
 public interface StarShipRepository extends CrudRepository<StarShip, Long> {
+    Iterable<StarShip> findByName(String name);
 }
